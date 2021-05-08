2fd68067135f624f056e2711db46f2abb64647d1
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-05-08 10:26:50 -0700

    rayas updates

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -8,5 +9,16 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Sets up the routes
+app.use (express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+app.use (bodyParser.urlencoded ({extended:true}));
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+  });
