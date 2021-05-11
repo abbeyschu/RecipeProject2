fe02338702ba4da7ab3dafc5ef97ea3437d0e477
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-05-10 22:02:28 -0700

    server.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const favoritesRoutes = require('./favoritesRoutes');
 
-router.use('/users', userRoutes);
-router.use('/favorites', favoritesRoutes);
+// router.use('/users', userRoutes);
+// router.use('/favorites', favoritesRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 
-const User = require('./User');
-const Favorites = require('./Favorites');
+// const User = require('./User');
+// const Favorites = require('./Favorites');
 
