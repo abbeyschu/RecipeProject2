72e6c363944e222a7f4b1f74f12014769c23f6db
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-05-12 21:32:53 -0700

    cookies

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# RecipeProject2
-This project 
+# Description
+Welcome to the Yummy Corner — a JavaScript recipe search engine application for all your cooking needs; from deciding what to make to the ingredients needed to the actual recipe of your dish. All while keeping UX & UI in focus.
+
+# Technologies used
+
+# Installation
+
+# Instructions
+
+# Screenshots
+
+# Contributors
+- Abbey Schueller - https://github.com/abbeyschu
+- Raya Alsaedi - https://github.com/rayaalsaedi
+- Matt Dragin - https://github.com/Mdragin
+- Perlum Toombs - https://github.com/ClarkT06
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 // Dependencies
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 // const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+    // Tells our session to use cookies
+
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // Sets up the routes
 app.use (express.static(__dirname+"/public"));
